4d47bdb603f4f03a8a5f8c43742117847b05ecc8
Damir <damir.keldibekov@gmail.com>
2014-09-06 21:39:23 +0400

    connect to mysql localhost

--- src/main/java/kz/app/UserLoginView.java
+++ src/main/java/kz/app/UserLoginView.java
@@ -1,17 +1,42 @@
 package kz.app;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.annotation.Resource;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 import org.primefaces.context.RequestContext;
 
+
 @ManagedBean
 public class UserLoginView {
+	
+	//@Resource(name="jdbc/mydb")
+	private DataSource ds;
+	
 	private String username;
     
     private String password;
+    
+    public UserLoginView() {
+    	try {
+			Context tx = new InitialContext();
+			ds = (DataSource) tx.lookup("java:comp/env/jdbc/mydb");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 
 	public String getUsername() {
 		return username;
@@ -30,6 +55,22 @@ public class UserLoginView {
 	}
 
 	public void login(ActionEvent event) {
+		try {
+			Connection con = ds.getConnection();
+			Statement stmt = con.createStatement();
+			String sql = "select surname from employee";
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				String sr = rs.getString("surname");
+				System.out.println("surname = " + sr);
+			}
+			rs.close();
+			stmt.close();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		RequestContext context = RequestContext.getCurrentInstance();
 		FacesMessage msg = null;
 		boolean logged = false;

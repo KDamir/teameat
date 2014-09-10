5d978c84a994f862bf0f8ffbefff6763be548530
Damir <damir.keldibekov@innoforce.kz>
2014-09-10 12:02:48 +0600

    Рефакторинг

---
+++ src/main/java/kz/app/dao/InvoiceDao.java
@@ -0,0 +1,12 @@
+package kz.app.dao;
+
+/**
+ * Created by Vlad Zheltovskiy.
+ * E-mail: vladislav.zheltovsky@inessoft.kz
+ * Skype: v.zheltovskiy
+ * <p/>
+ * Date: 09.09.14
+ * Time: 16:57
+ */
+public class InvoiceDao {
+}

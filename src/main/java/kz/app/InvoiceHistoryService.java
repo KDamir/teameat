46e62c04d0ceff65d87d4c96cb360cedc086bef1
Damir <damir.keldibekov@innoforce.kz>
2014-09-24 14:39:38 +0600

    Просмотр накладных

---
+++ src/main/java/kz/app/InvoiceHistoryService.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kz.app;
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import kz.app.dao.InvoiceDao;
+import kz.app.entity.InvoiceEntity;
+import kz.app.utils.HibernateUtil;
+
+/**
+ *
+ * @author damir.keldibekov
+ */
+@ManagedBean(name = "historyService")
+@RequestScoped
+public class InvoiceHistoryService {
+    private List<InvoiceEntity> listInvoice;
+    
+    private static final InvoiceDao dao = new InvoiceDao();
+    
+    @PostConstruct
+    public void init() {
+        HibernateUtil.getSession().beginTransaction();
+        listInvoice = dao.getListInvoice();
+        HibernateUtil.getSession().getTransaction().commit();
+    }
+
+    public List<InvoiceEntity> getListInvoice() {
+        return listInvoice;
+    }
+}
--- src/main/java/kz/app/dao/InvoiceDao.java
+++ src/main/java/kz/app/dao/InvoiceDao.java
@@ -1,6 +1,6 @@
 package kz.app.dao;
 
-import kz.app.Invoice;
+import kz.app.entity.InvoiceEntity;
 import kz.app.MeatPart;
 
 import java.util.List;
@@ -8,22 +8,23 @@ import kz.app.utils.HibernateUtil;
 import org.hibernate.Query;
 
 public class InvoiceDao {
-    public List<Invoice> getListInvoice() {
-        return HibernateUtil.getSessionfactory().getCurrentSession().createQuery("from InvoiceEntity").list();
+    public List<InvoiceEntity> getListInvoice() {
+        return HibernateUtil.getSessionfactory().getCurrentSession()
+                .createQuery("from InvoiceEntity").list();
     }
 
-    public void saveInvoice(Invoice inv) {
+    public void saveInvoice(InvoiceEntity inv) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public List<MeatPart> getListInvoicePart(Invoice inv) {
+    public List<MeatPart> getListInvoicePart(InvoiceEntity inv) {
         Query query = HibernateUtil.getSessionfactory().getCurrentSession().createQuery("from MeatPartEntity where idInvoice = :inv");
             query.setParameter("inv", inv.getId());
             List<MeatPart> list = query.list();
             return list;
     }
 
-    public void saveInvoicePart(Invoice inv) {
+    public void saveInvoicePart(InvoiceEntity inv) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
